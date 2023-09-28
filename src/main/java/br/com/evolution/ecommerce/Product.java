3c1d03846a095a5228257c3411cecf2530855c15
rondinelli-oliveira <rondinelliads@gmail.com>
2023-09-28 14:48:26 -0300

    mapping first class

---
+++ src/main/java/br/com/evolution/ecommerce/Product.java
@@ -0,0 +1,66 @@
+package br.com.evolution.ecommerce;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
