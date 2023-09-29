a16be136c5f34c4d7ceae7fd02a8afcfc558d791
rondinelli-oliveira <rondinelliads@gmail.com>
2023-09-29 10:28:58 -0300

    creating generic class for tests

---
+++ src/test/java/br/com/evolution/ecommerce/SearchingRecordsTest.java
@@ -0,0 +1,28 @@
+package br.com.evolution.ecommerce;
+
+import br.com.evolution.ecommerce.model.Product;
+import br.com.evolution.ecommerce.util.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchingRecordsTest extends EntityManagerTest{
+
+    @Test
+    public void findById() {
+        Product product = entityManager.find(Product.class, 1);
+//        Product product = entityManager.getReference(Product.class,1);
+
+        Assert.assertNotNull(product);
+        Assert.assertEquals("Kindle", product.getName());
+    }
+
+    @Test
+    public void updateReference() {
+        Product product = entityManager.find(Product.class, 1);
+        product.setName("Microphone Samson");
+
+        entityManager.refresh(product);
+
+        Assert.assertEquals("Kindle", product.getName());
+    }
+}
